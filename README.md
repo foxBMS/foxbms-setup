6990379056d781b1b9ae5dd6ac245b162a8ebea4
The foxBMS Team (Wachtler) <info@foxbms.org>
2018-06-12 14:40:01 +0200

    updated build scripts * updated waf script * updated README.md

--- README.md
+++ README.md
@@ -1,13 +1,24 @@
 # foxBMS
 
-foxBMS is a free, open and flexible development environment to design battery management systems. It is the first modular open source BMS development platform.
+foxBMS is a free, open and flexible development environment to design battery
+management systems. It is the first modular open source BMS development
+platform.
 
 ## foxBMS Project Setup
 The foxBMS project consists of several repositories.
 
-The foxConda environment. This environment provides all the tools necessary to generate the documentation, compile the code for the MCUs and flash the generated binaries on the MCUs (e.g., Python, git, GCC).
+The foxConda environment. This environment provides all the tools necessary
+ to generate the documentation, compile the code for the MCUs and flash the
+  generated binaries on the MCUs (e.g., Python, git, GCC).
 
-The starting point to get foxBMS is the foxBMS-setup repository (https://github.com/foxBMS/foxbms-setup), which contains the general setup files for the foxBMS project. It includes a setup script (bootstrap.py) which clones all the other needed repositories. The needed documentation will be generated automatically after these repositories have been cloned. The generated documentation is found in the directory ./build. After the bootstrap step, the top project directory (foxBMS-setup) structure looks like this:
+The starting point to get foxBMS is the foxBMS-setup repository 
+(https://github.com/foxBMS/foxbms-setup), which contains the general setup
+ files for the foxBMS project. It includes a setup script (bootstrap.py) 
+ which clones all the other needed repositories. The needed documentation 
+ will be generated automatically after these repositories have been cloned. 
+ The generated documentation is found in the directory ./build. After the 
+ bootstrap step, the top project directory (foxBMS-setup) structure looks 
+ like this:
 
  - foxbms-setup <dir>
   - .git <dir> *
@@ -29,28 +40,56 @@ The starting point to get foxBMS is the foxBMS-setup repository (https://github.
   - README.md <file>
   - wscript <file>
 
-* Directories and files with starting full stop are hidden in Windows in default configuration. 
+* Directories and files with starting full stop are hidden in Windows in 
+default configuration. 
 
 There is a help available by running "python bootstrap.py -h".
 
 ## foxBMS Repositories
 
-The foxbms-setup repositories have already been described.
+The foxbms-setup repository has already been described.
 
-foxBMS is made out of two Microcontroller Units (MCU), named primary and secondary. The C code for the primary MCU is found in the repository foxBMS-primary (https://github.com/foxBMS/mcu-primary). The C code for the secondary MCU is found in the repository foxBMS-secondary (https://github.com/foxBMS/mcu-secondary). The C code common to the primary and secondary MCU is found in the repository mcu-common (https://github.com/foxBMS/mcu-common).The Doxygen documentation is generated from these sources into ./build/primary/doxygen/html and ./build/secondary/doxygen/html respectively. The main file is in both cases index.html.
+foxBMS is made out of two Microcontroller Units (MCU), named primary and 
+secondary. The C code for the primary MCU is found in the repository 
+foxBMS-primary (https://github.com/foxBMS/mcu-primary). The C code for the 
+secondary MCU is found in the repository foxBMS-secondary 
+(https://github.com/foxBMS/mcu-secondary). The C code common to the primary 
+and secondary MCU is found in the repository mcu-common 
+(https://github.com/foxBMS/mcu-common).The Doxygen documentation is generated 
+from these sources into ./build/primary/doxygen/html and 
+./build/secondary/doxygen/html respectively. The main file is in both cases 
+index.html.
 
-The layout and schematic files for the foxBMS hardware are found in the foxBMS-hardware repository (https://github.com/foxBMS/hardware).
+The layout and schematic files for the foxBMS hardware are found in the 
+foxBMS-hardware repository (https://github.com/foxBMS/hardware).
 
-The Hardware Abstraction Layer (hal) for foxBMS is found in the hal-repository (https://github.com/foxBMS/mcu-hal). The real time operating system (FreeRTOS) for foxBMS is found in the FreeRTOS-repository (https://github.com/foxBMS/mcu-freertos).
+The Hardware Abstraction Layer (hal) for foxBMS is found in the hal-repository 
+(https://github.com/foxBMS/mcu-hal). The real time operating system (FreeRTOS) 
+for foxBMS is found in the FreeRTOS-repository 
+(https://github.com/foxBMS/mcu-freertos).
 
-The tools needed for foxBMS are in the foxBMS-tools-repository (https://github.com/foxBMS/tools.)
+The tools needed for foxBMS are in the foxBMS-tools-repository 
+(https://github.com/foxBMS/tools.)
 
-The general documentation files for the foxBMS project are found in the foxBMS-documentation repository (https://github.com/foxBMS/documentation). The sphinx documentation is found in foxBMS-documentation/doc/sphinx while the Doxygen documentation configuration is found in foxBMS-documentation/doc/doxygen. The Doxygen documentation itself is found in the software sources of the primary and secondary microcontroller. The general documentation, rendered from the sphinx sources is found in ./build/sphinx/foxBMS-documentation/doc/sphinx/html. The main file is index.html
+The general documentation files for the foxBMS project are found in the 
+foxBMS-documentation repository (https://github.com/foxBMS/documentation). The 
+sphinx documentation is found in foxBMS-documentation/doc/sphinx while the 
+Doxygen documentation configuration is found in 
+foxBMS-documentation/doc/doxygen. The Doxygen documentation itself is found in 
+the software sources of the primary and secondary microcontroller. The general 
+documentation, rendered from the sphinx sources is found in 
+./build/sphinx/foxBMS-documentation/doc/sphinx/html. 
+The main file is index.html
 
-A generated version of the Sphinx documentation can be found at http://foxbms.readthedocs.io/. It explains the structure of the foxBMS hardware, how to install the foxConda environment and how to use foxConda to compile and flash the sources.
+A generated version of the Sphinx documentation can be found at 
+http://foxbms.readthedocs.io/. It explains the structure of the foxBMS 
+hardware, how to install the foxConda environment and how to use foxConda 
+to compile and flash the sources.
 
 ## Building the Sources
-For building the software, open a shell and type "python build.py -h". All available build options will be displayed. The top build directory is ./build.
+For building the software, open a shell and type "python build.py -h". All 
+available build options will be displayed. The top build directory is ./build.
 
 ## Cleaning the ./build-Directory
-For cleaning instructions open a shell and type "python clean.py -h". All available cleaning options will be displayed.
+For cleaning instructions open a shell and type "python clean.py -h". 
+All available cleaning options will be displayed.
--- build.py
+++ build.py
@@ -1,14 +1,32 @@
-# @copyright &copy; 2010 - 2017, Fraunhofer-Gesellschaft zur Foerderung der angewandten Forschung e.V. All rights reserved.
+# @copyright &copy; 2010 - 2018, Fraunhofer-Gesellschaft zur Foerderung der
+#   angewandten Forschung e.V. All rights reserved.
 #
 # BSD 3-Clause License
-# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-# 1.  Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-# 2.  Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-# 3.  Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1.  Redistributions of source code must retain the above copyright notice,
+#     this list of conditions and the following disclaimer.
+# 2.  Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions and the following disclaimer in the documentation
+#     and/or other materials provided with the distribution.
+# 3.  Neither the name of the copyright holder nor the names of its
+#     contributors may be used to endorse or promote products derived from this
+#     software without specific prior written permission.
 #
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 #
-# We kindly request you to use one or more of the following phrases to refer to foxBMS in your hardware, software, documentation or advertising materials:
+# We kindly request you to use one or more of the following phrases to refer to
+# foxBMS in your hardware, software, documentation or advertising materials:
 #
 # &Prime;This product uses parts of foxBMS&reg;&Prime;
 #
@@ -26,24 +44,34 @@
 
 Helper script for building binaries and documentation of foxBMS
 """
-
 import os
 import sys
-import subprocess
 import argparse
 import logging
-import shutil
+import subprocess
 
-
-sys.dont_write_bytecode = True
-
-__version__ = 0.1
+__version__ = 0.2
 __date__ = '2017-11-29'
-__updated__ = '2017-11-29'
+__updated__ = '2018-01-25'
 
-TOOLCHAIN_BASIC_CONFIGURE = sys.executable + ' ' + \
-    os.path.join('tools', 'waf-1.9.13') + ' ' + 'configure'
-TOOLCHAIN_GCC_CHECK = '--check-c-compiler=gcc'
+
+def autodetect_waf():
+    waf_finder = os.path.join('tools', 'misc', 'autodetect_waf.py')
+    cmd = '{} {} -r'.format(sys.executable, waf_finder)
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = proc.communicate()
+    wversion, wpath = out.split(' ')
+    wpath = wpath.replace('\n', '')
+    wpath = wpath[1:-2] # remove leading and trailing braces
+    wpath = os.path.normpath(wpath)
+    logging.debug(wversion)
+    logging.debug(wpath)
+    return wpath
+
+
+def create_waf_run_string(waf_version, *args):
+    arg_string = ' '.join(args)
+    return '{} {} {}'.format(sys.executable, waf_version, arg_string)
 
 
 def start_process(cmd, supress_output=False):
@@ -72,160 +100,185 @@ def start_process(cmd, supress_output=False):
         sys.exit(1)
 
 
-def build(mcu_switch=None, doxygen=False, supress_output=False):
-    """Creates the build command string for the specified build and passes the
-    build command string to `start_process` which actually starts the build
-    process.
+def generate_cmd(variant, build=True, clean=False, doxygen=False):
+    jobs = []
+    if clean:
+        jobs.append('clean_{}'.format(variant))
+    if build:
+        jobs.append('build_{}'.format(variant))
+    if doxygen:
+        jobs.append('doxygen_{}'.format(variant))
+    return jobs
 
-    Args:
-        mcu_switch (string): specifies what will be built.
-        doxygen (bool): specifies if the doxygen documentation to a mcu should
-            be built.
-        supress_output (bool): indicates if the output should appear on the
-            command line.
+
+def main(args=None):
+    """Based on the input form command line the build/clean string is generated
+    and passed to waf.
     """
-    cmd = TOOLCHAIN_BASIC_CONFIGURE + ' '
-    if mcu_switch is None:
-        cmd += 'sphinx'
-    elif mcu_switch == '-p' or mcu_switch == '-s' or mcu_switch == '-b':
-        cmd += 'build' + ' ' + mcu_switch
-        if doxygen is True:
-            cmd += ' ' + 'doxygen'
-    else:
-        logging.error('Invalid build argument: \'%s\'', mcu_switch)
-        sys.exit(1)
-    start_process(cmd, supress_output)
+    program_name = os.path.basename(sys.argv[0])
+    program_version = '{}'.format(__version__)
+    program_build_date = str(__updated__)
+    program_version_message = '{} {}'.format(
+        program_version, program_build_date)
+    program_shortdesc = __import__('__main__').__doc__.split('\n')[1]
+    program_license = '''{}
 
-def styleguide(mcu_switch, supress_output=False):
-    cmd = '{} {} styleguide_function'.format(TOOLCHAIN_BASIC_CONFIGURE, mcu_switch)
-    start_process(cmd, supress_output=False)
+    Created by the foxBMS Team on {}.
+    Copyright 2018 foxBMS. All rights reserved.
 
-def build_wrapper(cmd_line_args):
-    b_pr = cmd_line_args.primary
-    b_se = cmd_line_args.secondary
-    b_bo = cmd_line_args.bootloader
+    Licensed under the BSD 3-Clause License.
 
-    if cmd_line_args.all:
-        build()
-        build(mcu_switch='-p')
-        build(mcu_switch='-p', doxygen=True)
-        build(mcu_switch='-s')
-        build(mcu_switch='-s', doxygen=True)
-        build(mcu_switch='-b')
-        build(mcu_switch='-b', doxygen=True)
-    elif cmd_line_args.sphinx:
-        build()
-    elif (b_pr and not b_se and not b_bo) or \
-         (not b_pr and b_se and not b_bo) or \
-         (not b_pr and not b_se and b_bo):
-        if cmd_line_args.primary:
-            mcu_switch = '-p'
-        elif cmd_line_args.secondary:
-            mcu_switch = '-s'
-        elif cmd_line_args.bootloader:
-            mcu_switch = '-b'
-        if cmd_line_args.doxygen:
-            build(mcu_switch, cmd_line_args.doxygen)
-        elif cmd_line_args.styleguide:
-            styleguide(mcu_switch)
-        else:
-            build(mcu_switch)
-    else:
-        logging.error('Build combination not valid')
+    Distributed on an "AS IS" basis without warranties
+    or conditions of any kind, either express or implied.
 
-def clean_wrapper(cmd_line_args):
-    c_pr = cmd_line_args.primary
-    c_se = cmd_line_args.secondary
-    c_bo = cmd_line_args.bootloader
-    if cmd_line_args.all:
-        clean()
-        clean(mcu_switch='-p')
-        clean(mcu_switch='-s')
-        clean(mcu_switch='-b')
-    elif cmd_line_args.sphinx:
-        clean()
-    elif (c_pr and not c_se and not c_bo) or \
-         (not c_pr and c_se and not c_bo) or \
-         (not c_pr and not c_se and c_bo):
-            if cmd_line_args.primary:
-                mcu_switch = '-p'
-            if cmd_line_args.secondary:
-                mcu_switch = '-s'
-            if cmd_line_args.bootloader:
-                mcu_switch = '-b'
-            clean(mcu_switch)
+USAGE
+'''.format(program_shortdesc, str(__date__))
 
-def clean(mcu_switch=None, supress_output=False):
-    cmd = TOOLCHAIN_BASIC_CONFIGURE +  ' '
-    if mcu_switch is None:
-        sphinx_build_dir = os.path.join('build', 'sphinx')
-        if os.path.isdir(sphinx_build_dir):
-            shutil.rmtree(sphinx_build_dir)
-            print "Successfully removed sphinx documentation"
-        else:
-            print 'Nothing to clean...'
-        return
-    elif mcu_switch == '-p' or mcu_switch == '-s' or  mcu_switch == '-b' :
-        cmd += ' ' + mcu_switch + ' ' + 'clean'
-    else:
-        print 'Invalid clean argument: \'{}\''.format(mcu_switch)
-        sys.exit(1)
-    start_process(cmd, supress_output)
-
-
-def main(cmd_line_args):
-    """Based on the input form command line the specified build string is
-    passed to the build/clean wrapper function.
-
-    Args:
-        cmd_line_args (Namespace): command line arguments
-    """
-    if cmd_line_args.clean:
-        clean_wrapper(cmd_line_args)
-    if cmd_line_args.nobuild == False:
-        build_wrapper(cmd_line_args)
-
-if __name__ == '__main__':
-    HELP_TEXT = """This script builds the software and documentation
-repositories based on the specified commands."""
     parser = argparse.ArgumentParser(
-        description=HELP_TEXT,
-        formatter_class=argparse.RawTextHelpFormatter,
-        add_help=True)
-    opt_args = parser.add_argument_group('optional arguments:')
-    opt_args.add_argument('-nobld', '--nobuild', default=False,
-                          action='store_true', required=False)
-    opt_args.add_argument('-sphi', '--sphinx', action='store_true',
-                          required=False, help='builds sphinx documenation')
-    opt_args.add_argument('-p', '--primary', action='store_true',
-                          required=False, help='builds primary binaries')
-    opt_args.add_argument('-s', '--secondary', action='store_true',
-                          required=False, help='builds secondary binaries')
-    opt_args.add_argument('-b', '--bootloader', action='store_true',
-                          required=False, help='builds bootloader binaries')
-    opt_args.add_argument(
+        description=program_license,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument(
+        '-v',
+        '--verbosity',
+        dest='verbosity',
+        action='count',
+        help='set verbosity level')
+    parser.add_argument(
+        '-V',
+        '--version',
+        action='version',
+        version=program_version_message)
+
+    bld_args = parser.add_argument_group('build arguments')
+
+    bld_args.add_argument(
+        '-c',
+        '--conf',
+        action='store_true',
+        required=False,
+        help='configures the project')
+
+    build_tgt = {'-p': '--primary',
+                 '-s': '--secondary',
+                 '-b': '--bootloader'}
+    for _short, _long in build_tgt.items():
+        bld_args.add_argument(_short, _long,
+                              action='store_true',
+                              required=False,
+                              help='builds {} binaries'.format(_long[2:]))
+
+    bld_args.add_argument(
         '-dox',
         '--doxygen',
         action='store_true',
         required=False,
-        help='builds the software documentation for the specified mcu (\'-p\'\
-        ,\'-s\') of the bootloader (\'-b\')')
-    opt_args.add_argument(
+        help='builds the software documentation for the specified mcu {}\
+'.format(build_tgt.items()))
+
+    bld_args.add_argument(
+        '-sphi',
+        '--sphinx',
+        action='store_true',
+        required=False,
+        help='builds the general sphinx documenation')
+
+    bld_args.add_argument(
         '-a',
         '--all',
         action='store_true',
         required=False,
         help='generates all of the above mentioned')
-    opt_args.add_argument('-v', '--verbose', action='store_true',
-                          required=False, help='show diagnostic output')
-    opt_args.add_argument('-sg', '--styleguide', action='store_true',
-                          required=False, help='run styleguide function only')
-    opt_args.add_argument('--clean', action='store_true',
-                          required=False, help='cleans the specified option')
-    CMD_LINE_ARGS = parser.parse_args()
-    if CMD_LINE_ARGS.verbose:
+
+    bld_args.add_argument(
+        '--clean',
+        action='store_true',
+        required=False,
+        help='cleans the specified mcu binaries and documentation {} or \
+general sphinx documentation'.format(build_tgt.items()))
+
+    bld_args.add_argument(
+        '-dc',
+        '--distclean',
+        action='store_true',
+        required=False,
+        help='Removes the build directory and lock file. The project is \
+reconfigured afterwards')
+
+    bld_args.add_argument(
+        '-nobld',
+        '--nobuild',
+        default=False,
+        action='store_true',
+        required=False,
+        help='If specified, then no rebuild after cleaning will be triggered.')
+
+    args = parser.parse_args(args)
+
+    if args.verbosity == 1:
+        logging.basicConfig(level=logging.INFO)
+    elif args.verbosity > 1:
         logging.basicConfig(level=logging.DEBUG)
     else:
-        logging.basicConfig(level=logging.INFO)
+        logging.basicConfig(level=logging.ERROR)
 
-    main(CMD_LINE_ARGS)
+    rebuild = True
+    if args.nobuild:
+        rebuild = False
+
+    used_waf_version = autodetect_waf()
+
+    run_list = []
+    if args.distclean:
+        run_list.append('distclean')
+    run_list.append('configure')
+
+    if args.all:
+        args.primary = True
+        args.secondary = True
+        args.bootloader = False
+        args.doxygen = True
+        args.sphinx = True
+
+    if args.primary:
+        run_list.extend(
+            generate_cmd(
+                'primary',
+                build=rebuild,
+                clean=args.clean,
+                doxygen=args.doxygen))
+    if args.secondary:
+        run_list.extend(
+            generate_cmd(
+                'secondary',
+                build=rebuild,
+                clean=args.clean,
+                doxygen=args.doxygen))
+    if args.bootloader:
+        run_list.extend(
+            generate_cmd(
+                'bootloader',
+                build=rebuild,
+                clean=args.clean,
+                doxygen=args.doxygen))
+
+    if args.sphinx:
+        run_list.append('sphinx')
+    logging.info('Building following targets:')
+    for targ in run_list[1:]:
+        logging.info('  - {}'.format(targ))
+
+    exec_cmd = [
+        create_waf_run_string(
+            used_waf_version,
+            targ) for targ in run_list]
+    logging.info('Created run string for following targets:')
+    for _cmd in exec_cmd:
+        logging.info('  - {}'.format(_cmd))
+
+    for _cmd in exec_cmd:
+        start_process(_cmd)
+
+
+if __name__ == '__main__':
+    main()
